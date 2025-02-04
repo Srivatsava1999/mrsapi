dd870c5a0f2340a3d77b3ab5ae4f2cdbc64fa8ad
Srivatsava1999 <srivatsava.dhanala@gmail.com>
2025-02-04 13:18:06 -0800

    Create MovieDirectory and TheatreDirectory Models and Migrated them

--- mrsapi/movie_reservation/Movie/models.py
+++ mrsapi/movie_reservation/Movie/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MovieDirectory(models.Model):
+    movieId=models.AutoField(primary_key=True)
+    movieTitle=models.CharField(max_length=100)
+    duration=models.DurationField(help_text="Duration of the movie (HH:MM:SS format)")
+    rating=models.CharField(max_length=75)
+    release_date=models.DateField(max_length=75)
+
+    def __str__(self):
+        return self.movieTitle
--- mrsapi/movie_reservation/Theatre/models.py
+++ mrsapi/movie_reservation/Theatre/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TheatreDirectory(models.Model):
+    theatreId=models.AutoField(primary_key=True)
+    theatreName=models.CharField(max_length=75)
+    address=models.CharField(max_length=100)
+    locationCity=models.CharField(max_length=75)
+    locationState=models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.theatreName
