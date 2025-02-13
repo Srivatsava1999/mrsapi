b315ca5151cf2d14becba77084dc9d87a8c9f0c6
Srivatsava1999 <srivatsava.dhanala@gmail.com>
2025-02-12 21:44:41 -0800

    Final commit of the day

--- mrsapi/movie_reservation/Booking/models.py
+++ mrsapi/movie_reservation/Booking/models.py
@@ -32,6 +32,22 @@ class ShowDirectory(models.Model):
     dateTime=models.DateTimeField()
     houseFullFlag=models.BooleanField(default=False)
 
+    class Meta:
+        constraints=[models.UniqueConstraint(
+            fields=['screenId','dateTime','showTypeId'],
+            name='unique_show_per_screen_per_date'
+        )]
+    
+    def save(self, *args, **kwargs):
+        if ShowDirectory.objects.filter(
+            screenId=self.screenId,
+            dateTime=self.dateTime,
+            showTypeId=self.showTypeId
+            ).exists():
+                raise IntegrityError("Show already scheduled for this screem on this data.")
+        super().save(*args,**kwargs)
+        
+
 class BookingDirectory(models.Model):
     bookingId=models.AutoField(primary_key=True)
     seatID=models.ForeignKey(SeatMaster, on_delete=models.CASCADE)
